d04a739ec51162560202c55602e7bc4088f83f9a
maryson gleison <maryson011@gmail.com>
2024-08-21 17:02:02 -0300

    feat: ensure recommended bookings are shown randomly

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -27,8 +27,13 @@ export default async function Home() {
 // }) : []
 
   // em paralelo
-  const [ barbershops, confimedBookings ] = await Promise.all([
+  const [ barbershops, recomendedBarbershops, confimedBookings ] = await Promise.all([
     db.barbershop.findMany({}),
+    db.barbershop.findMany({
+      orderBy: {
+        id: 'asc'
+      }
+    }),
     session?.user
     ? db.booking.findMany({
         where: {
@@ -94,7 +99,7 @@ export default async function Home() {
         </h2>
 
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-          {barbershops.map((barbershop) => (
+          {recomendedBarbershops.map((barbershop) => (
             <div key={barbershop.id} className="min-w-[167px] max-w-[167px]">
               <BarbershopItem key={barbershop.id} barbershop={barbershop} />
             </div>
